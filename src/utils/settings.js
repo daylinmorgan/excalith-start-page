842d8b2e5426a509efe79e820cb413a07a5ccf75
Daylin Morgan <daylinmorgan@gmail.com>
2023-02-21 19:12:41 -0600

    CONFIG CONFIG CONFIG

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,39 +1,68 @@
+const mocha = {
+	rosewater: "#f5e0dc",
+	flamingo: "#f2cdcd",
+	pink: "#f5c2e7",
+	mauve: "#cba6f7",
+	red: "#f38ba8",
+	maroon: "#eba0ac",
+	peach: "#fab387",
+	yellow: "#f9e2af",
+	green: "#a6e3a1",
+	teal: "#94e2d5",
+	sky: "#89dceb",
+	sapphire: "#74c7ec",
+	blue: "#89b4fa",
+	lavender: "#b4befe",
+	text: "#cdd6f4",
+	subtext1: "#bac2de",
+	subtext0: "#a6adc8",
+	overlay2: "#9399b2",
+	overlay1: "#7f849c",
+	overlay0: "#6c7086",
+	surface2: "#585b70",
+	surface1: "#45475a",
+	surface0: "#313244",
+	base: "#1e1e2e",
+	mantle: "#181825",
+	crust: "#11111b"
+}
+
 const defaultConfig = {
-	username: "Excalith",
+	username: "daylin",
 	terminal: {
 		fixedHeight: true,
-		backgroundColor: "#121317",
-		windowColor: "#1e212b",
-		glowColor: "#6b5cb157"
+		backgroundColor: mocha.mantle,
+		windowColor: mocha.base,
+		glowColor: mocha.mantle
 	},
 	prompt: {
 		ctrlC: true,
 		placeholder: "command...",
 		userColor: "green",
-		atColor: "gray",
+		atColor: "white",
 		hostColor: "magenta",
 		promptColor: "magenta",
 		promptSymbol: "❯",
 		caretColor: "green",
-		selectionBg: "#e8b195",
-		selectionFg: "#16161e"
+		selectionBg: mocha.rosewater,
+		selectionFg: mocha.sky
 	},
 	colors: {
-		white: "#e2e2e2",
-		gray: "#97989d",
-		black: "#16161e",
-		red: "#ec6183",
-		green: "#2ed8a2",
-		yellow: "#e8b195",
-		blue: "#2bc3de",
-		cyan: "#62e0e2",
-		magenta: "#e069aa",
-		purple: "#d1aff8",
-		orange: "#ff8800"
+		gray: mocha.surface0,
+		white: mocha.text,
+		black: mocha.mantle,
+		red: mocha.red,
+		green: mocha.green,
+		yellow: mocha.yellow,
+		blue: mocha.blue,
+		cyan: mocha.sky,
+		magenta: mocha.flamingo,
+		purple: mocha.mauve,
+		orange: mocha.peach
 	},
 	fetch: {
 		timeFormat: "HH:mm",
-		dateFormat: "DD/MM/YYYY",
+		dateFormat: "YYYY.MM.DD",
 		titleColor: "yellow"
 	},
 	urlLaunch: {
@@ -50,24 +79,9 @@ const defaultConfig = {
 				url: "https://google.com/search?q={}"
 			},
 			{
-				alias: "d",
-				name: "DuckDuckGo Search",
-				url: "https://duckduckgo.com/?q={}"
-			},
-			{
-				alias: "b",
-				name: "Brave Search",
-				url: "https://search.brave.com/search?q={}"
-			},
-			{
 				alias: "gh",
 				name: "Github Search",
 				url: "https://github.com/search?q={}"
-			},
-			{
-				alias: "s",
-				name: "Stack Overflow Search",
-				url: "https://stackoverflow.com/search?q={}"
 			},
 			{
 				alias: "r",
@@ -79,35 +93,24 @@ const defaultConfig = {
 				name: "Wikipedia Search",
 				url: "https://en.wikipedia.org/wiki/{}"
 			}
+			// {
+			// 	alias: "d",
+			// 	name: "DuckDuckGo Search",
+			// 	url: "https://duckduckgo.com/?q={}"
+			// },
+			// {
+			// 	alias: "b",
+			// 	name: "Brave Search",
+			// 	url: "https://search.brave.com/search?q={}"
+			// },
+			// {
+			// 	alias: "s",
+			// 	name: "Stack Overflow Search",
+			// 	url: "https://stackoverflow.com/search?q={}"
+			// },
 		]
 	},
 	sections: [
-		{
-			title: "General",
-			color: "green",
-			links: [
-				{
-					name: "Portfolio",
-					url: "https://cancellek.com",
-					icon: "mdi:web"
-				},
-				{
-					name: "Keybase",
-					url: "https://keybase.io/",
-					icon: "fa-brands:keybase"
-				},
-				{
-					name: "GPT",
-					url: "https://chat.openai.com/",
-					icon: "simple-icons:openai"
-				},
-				{
-					name: "OCI",
-					url: "https://www.oracle.com/cloud/",
-					icon: "simple-icons:oracle"
-				}
-			]
-		},
 		{
 			title: "Dev",
 			color: "magenta",
@@ -123,14 +126,25 @@ const defaultConfig = {
 					icon: "ph:gitlab-logo-simple-fill"
 				},
 				{
-					name: "Dev.to",
-					url: "https://dev.to",
-					icon: "material-symbols:logo-dev"
+					name: "Daylin's Git Server",
+					url: "https://git.dayl.in",
+					icon: "mdi:github"
+				}
+			]
+		},
+		{
+			title: "Storage",
+			color: "orange",
+			links: [
+				{
+					name: "Drive",
+					url: "https://drive.google.com",
+					icon: "mdi:google-drive"
 				},
 				{
-					name: "Stack Overflow",
-					url: "https://stackoverflow.com/",
-					icon: "mdi:stack-overflow"
+					name: "Box",
+					url: "https://utbox.utexas.edu",
+					icon: "carbon:volume-file-storage"
 				}
 			]
 		},
@@ -144,97 +158,20 @@ const defaultConfig = {
 					icon: "mdi:twitter"
 				},
 				{
-					name: "Mastodon",
-					url: "https://mastodon.social/",
-					icon: "ri:mastodon-fill"
-				},
-				{
 					name: "Reddit",
 					url: "https://reddit.com",
 					icon: "mdi:reddit"
-				},
-				{
-					name: "Polywork",
-					url: "https://polywork.com",
-					icon: "simple-icons:polywork"
 				}
 			]
 		},
 		{
-			title: "Gaming",
-			color: "cyan",
+			title: "Lab",
+			color: "green",
 			links: [
 				{
-					name: "Polygon",
-					url: "https://polygon.com",
-					icon: "uil:polygon"
-				},
-				{
-					name: "IGN",
-					url: "https://ign.com",
-					icon: "mdi:currency-sign"
-				},
-				{
-					name: "RPS",
-					url: "https://rockpapershotgun.com/",
-					icon: "ph:toilet-paper-bold"
-				},
-				{
-					name: "80lv",
-					url: "https://80.lv/",
-					icon: "tabler:hand-rock"
-				}
-			]
-		},
-		{
-			title: "Science",
-			color: "blue",
-			links: [
-				{
-					name: "PopSci",
-					url: "https://popsci.com/",
-					icon: "material-symbols:science"
-				},
-				{
-					name: "Space",
-					url: "fa6-solid:user-astronaut",
-					icon: "mdi:reddit"
-				},
-				{
-					name: "NASA",
-					url: "https://blogs.nasa.gov/",
-					icon: "simple-icons:nasa"
-				},
-				{
-					name: "ESA",
-					url: "https://blogs.esa.int/",
-					icon: "mdi:black-mesa"
-				}
-			]
-		},
-		{
-			title: "Tech",
-			color: "yellow",
-			links: [
-				{
-					name: "TechCrunch",
-					url: "https://techcrunch.com/",
-					icon: "game-icons:techno-heart"
-				},
-				{
-					name: "Verge",
-					url: "https://www.theverge.com/",
-					icon: "arcticons:verge"
-				},
-				{
-					name: "It's Foss",
-					url: "https://itsfoss.com/",
-					icon: "ri:mastodon-fill"
-				},
-				{
-					name: "9To5 Linux",
-					url: "https://9to5linux.com/",
-					icon: "uil:linux"
+					name: "ClonMapper Protocol",
+					url: "https://docs.brocklab.com/clonmapper",
+					icon: "mdi:dna"
 				}
 			]
 		}
